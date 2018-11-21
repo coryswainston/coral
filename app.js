e3ff47b13d55b0cd659754102862732a334ca410
coryswainston <swainston.cory89@gmail.com>
2018-11-21 01:58:44 -0700

    Overhaul

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ app.set('port', (process.env.PORT || 5000))
   .set('view engine', 'ejs')
   .use(express.static(__dirname + '/public'))
   .get('/', (req, res) => res.render('pages/index'))
+  .get('/bio', (req, res) => res.render('pages/bio'))
+  .get('*', (req, res) => res.render('pages/404'))
   .listen(app.get('port'), () => {
     console.log("Node app is running at localhost:" + app.get('port'))
   });
